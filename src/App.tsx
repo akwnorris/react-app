eda00a0ff463d4b84c286031567ed6fc5f55b43f
Alex Norris <akwnorris@gmail.com>
2024-01-10 14:09:59 -0600

    Add Alert component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div>
+      <Alert>My alert</Alert>
       <Button onClick={() => console.log("Clicked")}>My Button</Button>
     </div>
   );
